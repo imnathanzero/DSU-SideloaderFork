b3716a87b467c4a7f01aa4ec33e6a2c1f8c41ec6
Nathan <nathannxx@proton.me>
2026-08-25 13:07:06 +0700

    Add preserve userdata UI

--- app/src/main/java/vegabobo/dsusideloader/ui/cards/UserdataCard.kt
+++ app/src/main/java/vegabobo/dsusideloader/ui/cards/UserdataCard.kt
@@ -17,50 +17,98 @@ import androidx.compose.ui.unit.sp
 import vegabobo.dsusideloader.R
 import vegabobo.dsusideloader.ui.components.CardBox
 import vegabobo.dsusideloader.ui.components.FileSelectionBox
+import vegabobo.dsusideloader.ui.components.PreferenceItem
 import vegabobo.dsusideloader.ui.screen.home.UserDataCardState
 
 @Composable
 fun UserdataCard(
     isEnabled: Boolean,
     uiState: UserDataCardState,
+    isDsuInstalled: Boolean,
     modifier: Modifier = Modifier,
     onValueChange: (String) -> Unit,
     onCheckedChange: (Boolean) -> Unit = {},
+    onPreserveCheckedChange: (Boolean) -> Unit = {},
 ) {
-    CardBox(
-        modifier = modifier,
-        cardTitle = stringResource(id = R.string.userdata_size),
-        addToggle = true,
-        isToggleEnabled = !isEnabled,
-        isToggleChecked = uiState.isSelected,
-        onCheckedChange = onCheckedChange,
-    ) {
-        AnimatedVisibility(
-            visible = uiState.isSelected,
-            enter = expandVertically(),
-            exit = shrinkVertically(),
+    if (isDsuInstalled && !isEnabled) {
+        CardBox(
+            modifier = modifier,
+            cardTitle = stringResource(id = R.string.userdata_size),
+            addToggle = false,
         ) {
             Column {
-                FileSelectionBox(
-                    modifier = Modifier.padding(bottom = 4.dp),
+                PreferenceItem(
+                    title = stringResource(id = R.string.preserve_userdata),
+                    description = stringResource(id = R.string.preserve_userdata_desc),
+                    showToggle = true,
+                    isChecked = uiState.preserveSelected,
                     isEnabled = !isEnabled,
-                    isError = uiState.isError,
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                    textFieldValue = uiState.text,
-                    textFieldTitle = stringResource(id = R.string.userdata_size_info),
-                    onValueChange = onValueChange,
+                    onClick = { onPreserveCheckedChange(!uiState.preserveSelected) },
                 )
-                AnimatedVisibility(visible = uiState.isError) {
-                    Text(
-                        modifier = Modifier.padding(start = 1.dp),
-                        text = stringResource(
-                            id = R.string.allowed_userdata_allocation,
-                            uiState.maximumAllowed,
-                        ),
-                        color = MaterialTheme.colorScheme.error,
-                        lineHeight = 14.sp,
-                        fontSize = 14.sp,
+
+                AnimatedVisibility(visible = !uiState.preserveSelected) {
+                    Column {
+                        FileSelectionBox(
+                            modifier = Modifier.padding(top = 8.dp, bottom = 4.dp),
+                            isEnabled = !isEnabled,
+                            isError = uiState.isError,
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                            textFieldValue = uiState.text,
+                            textFieldTitle = stringResource(id = R.string.userdata_size_info),
+                            onValueChange = onValueChange,
+                        )
+                        AnimatedVisibility(visible = uiState.isError) {
+                            Text(
+                                modifier = Modifier.padding(start = 1.dp),
+                                text = stringResource(
+                                    id = R.string.allowed_userdata_allocation,
+                                    uiState.maximumAllowed,
+                                ),
+                                color = MaterialTheme.colorScheme.error,
+                                lineHeight = 14.sp,
+                                fontSize = 14.sp,
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    } else {
+        CardBox(
+            modifier = modifier,
+            cardTitle = stringResource(id = R.string.userdata_size),
+            addToggle = true,
+            isToggleEnabled = !isEnabled,
+            isToggleChecked = uiState.isSelected,
+            onCheckedChange = onCheckedChange,
+        ) {
+            AnimatedVisibility(
+                visible = uiState.isSelected,
+                enter = expandVertically(),
+                exit = shrinkVertically(),
+            ) {
+                Column {
+                    FileSelectionBox(
+                        modifier = Modifier.padding(bottom = 4.dp),
+                        isEnabled = !isEnabled,
+                        isError = uiState.isError,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        textFieldValue = uiState.text,
+                        textFieldTitle = stringResource(id = R.string.userdata_size_info),
+                        onValueChange = onValueChange,
                     )
+                    AnimatedVisibility(visible = uiState.isError) {
+                        Text(
+                            modifier = Modifier.padding(start = 1.dp),
+                            text = stringResource(
+                                id = R.string.allowed_userdata_allocation,
+                                uiState.maximumAllowed,
+                            ),
+                            color = MaterialTheme.colorScheme.error,
+                            lineHeight = 14.sp,
+                            fontSize = 14.sp,
+                        )
+                    }
                 }
             }
         }
