bc6094906905acc35084229680858dbd9cbc7b13
Nathan <nathannxx@proton.me>
2026-09-05 19:45:32 +0700

    Clean unused import after about alignment fix

--- app/src/main/java/vegabobo/dsusideloader/ui/screen/about/AboutScreen.kt
+++ app/src/main/java/vegabobo/dsusideloader/ui/screen/about/AboutScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.flow.collectLatest
 import vegabobo.dsusideloader.R
