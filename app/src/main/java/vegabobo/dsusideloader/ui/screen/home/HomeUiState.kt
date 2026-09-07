64f28b17233ccd46d2b9a2e9e22e4705d6406fcc
Nathan <nathannxx@proton.me>
2026-09-08 06:19:07 +0700

    fix: remove remaining image size UI state

--- app/src/main/java/vegabobo/dsusideloader/ui/screen/home/HomeUiState.kt
+++ app/src/main/java/vegabobo/dsusideloader/ui/screen/home/HomeUiState.kt
@@ -22,11 +22,6 @@ data class UserDataCardState(
     val preserveSelected: Boolean = false,
 )
 
-data class ImageSizeCardState(
-    val isSelected: Boolean = false,
-    val text: String = "",
-)
-
 enum class AdditionalCardState {
     NONE,
     SETUP_STORAGE,
@@ -39,7 +34,6 @@ enum class AdditionalCardState {
 
 enum class SheetDisplayState {
     NONE,
-    IMAGESIZE_WARNING,
     CONFIRM_INSTALLATION,
     CANCEL_INSTALLATION,
     DISCARD_DSU,
@@ -49,7 +43,6 @@ enum class SheetDisplayState {
 data class HomeUiState(
     val installationCard: InstallationCardState = InstallationCardState(),
     val userDataCard: UserDataCardState = UserDataCardState(),
-    val imageSizeCard: ImageSizeCardState = ImageSizeCardState(),
     val additionalCard: AdditionalCardState = AdditionalCardState.NONE,
     val sheetDisplay: SheetDisplayState = SheetDisplayState.NONE,
     val installationLogs: String = "",
